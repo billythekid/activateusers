6c79da1b4becf5afae56830418ec7198147c2dc4
carlcs <carlcs@users.noreply.github.com>
2017-06-23 21:01:38 +0200

    Added German translations

---
+++ translations/de.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Activate Users plugin for Craft CMS
+ * Activate Users Translation
+ *
+ * @author    Billy Fagan
+ * @copyright Copyright (c) 2017 Billy Fagan
+ * @link      https://billyfagan.co.uk
+ * @package   ActivateUsers
+ * @since     1.0.0
+ */
+return array(
+    'Activate Users'                                                                         => 'Activate Users',
+    'Plugin that adds "Activate account" permissions so that non-admins can activate users.' => 'Plugin that adds "Activate account" permissions so that non-admins can activate users.',
+    'Activate Accounts'                                                                      => 'Benutzerkonten aktivieren',
+);
